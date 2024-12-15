2f06b205909fa4a20a4338ae7b3958790a798285
vicki-wu <vwu2003@gmail.com>
2024-12-15 17:04:26 -0500

    updated files

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://vicki:1234@memorykeeper.eve4x.mongodb.net/memorykeeper?retryWrites=true&w=majority"
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
